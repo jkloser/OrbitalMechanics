25ab0f30c983cba4162dc8d47c3e36fa51a6ea45
John Kloser <john.kloser3@gmail.com>
2021-02-11 13:20:59 -0600

    User input enabled

--- OrbitalMechanics.py
+++ OrbitalMechanics.py
@@ -3,7 +3,7 @@ import numpy as np
 from class_orbit import orbit
 import central_bodies
 
-"""
+
 ### Enter initial radius and velocity
 rx = float(input('Enter rx value: ').replace('\U00002013', '-'))
 ry = float(input('Enter ry value: ').replace('\U00002013', '-'))
@@ -13,10 +13,11 @@ vy = float(input('Enter vy value: ').replace('\U00002013', '-'))
 vz = float(input('Enter vz value: ').replace('\U00002013', '-'))
 r0 = np.array([rx, ry, rz])
 v0 = np.array([vx, vy, vz])
-"""
 
+"""
 r0 = np.array([-6045, -3490, 2500])
 v0 = np.array([-3.457, 6.618, 2.533])
+"""
 
 DU = 6378.145
 DUTU = 7.905368
